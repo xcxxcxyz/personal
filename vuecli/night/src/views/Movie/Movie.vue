7e8a5adb4b200b6d5b4db64f6961b2fe78c89bbc
xcxxcxyz <1294448317@qq.com>
2019-08-04 16:56:52 +0800

    admin

--- vuecli/night/src/views/Movie/Movie.vue
+++ vuecli/night/src/views/Movie/Movie.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="Movie">
-    <h1>电影</h1>
+	  <div id="pic">
+    <img src="https://img3.doubanio.com/view/photo/m_ratio_poster/public/p2563780504.jpg" alt="0">
+	</div>
+	<div id="word">
+	<span class="name">哪吒之魔童降世</span><br/>
+	<span class="person">吕艳婷/囧森瑟夫/瀚墨</span><br/>
+	<span class="date">2019年</span>
+	</div>
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
 
@@ -16,16 +22,54 @@ export default {
       let axurl = `https://bird.ioliu.cn/v2?url=`;
       let urlapi = `https://m.douban.com/rexxar/api/v2/subject_collection/movie_showing/items?start=0&count=10`;
       axios.get(axurl + urlapi).then(res => {
-        console.log(res);
+        console.log(res.data.subject_collection_items);
       });
     }
   },
   data() {
     return {
-      dataList: []
+      dateList: [
+
+	  ]
     };
   },
   name: "Movie",
   components: {}
 };
 </script>
+
+<style scoped>
+	*{
+		margin: 0;
+		padding: 0;
+	}
+	#pic{
+		width: 40%;
+		margin-left: 30px;
+		float: left;
+	}
+	.Movie{
+		width: 100%;
+		margin: 30px auto;
+		height: 200px;
+		line-height: 25px;
+		border-bottom: 1px solid black;
+	}
+	.word{
+		float: right;
+		/* margin-left: 40px; */
+	}
+	.name{
+		font-size: .3rem;
+	}
+	.person{
+		font-size: .25rem;
+	}
+	.date{
+		font-size: .25rem;
+	}
+	#pic img{
+		width: 100px;
+		height: 150px;	
+	}
+</style>
